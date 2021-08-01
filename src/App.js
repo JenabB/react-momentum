8b58db79197c3a15a7b037217dcedadb3aa2f39d
JenabB <birulangit1996@gmail.com>
2021-08-01 16:30:11 +0700

    change logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { useState, useEffect } from "react";
-import getImage from "./components/getImage";
+import { createClient } from "pexels";
+
 import Time from "./components/Time";
 import User from "./components/User";
 import Weather from "./components/Weather";
 
 const App = () => {
-  const [image, setImage] = useState([]);
+  const [condition, setCondition] = useState(1420440);
+  const [backgorund, setBackground] = useState([]);
+  const client = createClient(
+    "563492ad6f91700001000001f4437833d1ce4ef58f80595aa44d5417"
+  );
+
   useEffect(() => {
-    getImage().then((data) => setImage(data.src.landscape));
-  }, []);
+    if (new Date().getHours() <= 19 && new Date().getHours() >= 15) {
+      setCondition(1420440);
+    } else if (new Date().getHours() > 21 || new Date().getHours() <= 4) {
+      setCondition(2129796);
+    } else if (new Date().getHours() > 4 && new Date().getHours() < 12) {
+      setCondition(3030459);
+    } else if (new Date().getHours() < 15 && new Date().getHours() >= 12) {
+      setCondition(1174108);
+    }
+    client.photos.show({ id: condition }).then((photo) => {
+      setBackground(photo.src.landscape);
+    });
+  }, [client.photos, condition]);
+
   return (
     <div
       className="h-screen"
-      style={{ backgroundImage: `url(${image})`, backgroundSize: "cover" }}
+      style={{ backgroundImage: `url(${backgorund})`, backgroundSize: "cover" }}
     >
       <Weather />
       <Time />
